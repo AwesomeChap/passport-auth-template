e8ddd4df064b863e24c63013c5f7f6e63c46a63e
Jatin Kumar <jatn15011999@gmail.com>
2018-04-07 20:56:55 +0530

    handled auth

--- app.js
+++ app.js
@@ -4,12 +4,19 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+require('./config/passport')(passport);
+
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/',(req,res)=>{
    res.send('It Works!');
 });
+
+app.use('/auth',auth);
 
 const port = process.env.PORT || 8080;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI : 'mongodb://AwesomeChap:jatin1999@ds137089.mlab.com:37089/story-books',
+    googleClientID : '661841487222-3vmlte1lv02cmghe1fs5gecitif78ddo.apps.googleusercontent.com',
+    googleClientSecret : '3HPwFFUFQFh9fSF691FEtkOU'
+};
